920703525f7f6fcb6e2ff09a2f7727bacd467223
onifade617 <onifade617@gmail.com>
2024-08-19 10:04:56 +0100

    Update penguins.py

--- penguins.py
+++ penguins.py
@@ -7,3 +7,14 @@ Created on Mon Aug 19 09:51:05 2024
 
 import pandas as pd
 import streamlit as st
+import altair as alt
+import seaborn as sns
+
+
+st.title("Palmer's Penguins")
+
+#Read the csv file using pandas library
+df = pd.read_csv("penguins.csv")
+
+#Have a glimpse of the dataset
+st.write(df.head(10))
